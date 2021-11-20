c138e74cf226eb5c3bcdefc67170b724053aa82f
SeongMin-K <eo1tks3@naver.com>
2021-11-20 21:52:18 +0900

    Initial Commit

---
+++ SwiftUI_Tutorial_01/SwiftUI_Tutorial_01App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Tutorial_01App.swift
+//  SwiftUI_Tutorial_01
+//
+//  Created by SeongMinK on 2021/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Tutorial_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
